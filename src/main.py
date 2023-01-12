7a7c924490427861066d4adace74919aef9661ed
lannguyen <18120051@student.hcmus.edu.vn>
2023-01-12 23:29:31 +0700

    15. Rename logging folders

--- src/main.py
+++ src/main.py
@@ -4,8 +4,9 @@ from database import main as database_main
 from etl import main as etl_main
 
 # Configure logger
-logger.add("log/info.log", rotation="1 MB", retention="10 days", level="INFO")
-logger.add("log/debug.log", rotation="1 MB", retention="10 days", level="DEBUG")
+logger.add("logs/info.log", rotation="1 MB", retention="10 days", level="INFO")
+logger.add("logs/debug.log", rotation="1 MB",
+           retention="10 days", level="DEBUG")
 
 if __name__ == "__main__":
     database_main()
